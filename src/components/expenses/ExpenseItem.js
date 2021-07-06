54d6943f4d0f1e59326fa4e9fba87ca638b9859f
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2021-07-06 12:17:32 +0100

    Used composition and restructure project folders

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,20 +1,17 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2021, 6, 3);
-  // const expensePrice = 295.25;
-  // const expenseTitle = "Car Insurance";
-
   return (
     // This makes use of props
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2> {props.title} </h2>
-        <div className="expense-item__price">${props.amount} </div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
